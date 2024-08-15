50833452653ec700b0ab87ed6e91c3c27ddebd58
Shankhesh Ambaliya <159709569+Hitman-45@users.noreply.github.com>
2024-08-15 22:46:07 +0530

    New Password and Confirm Password should be same and bake navigation button

--- lib/screens/create_new_password.dart
+++ lib/screens/create_new_password.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:global_health_insights/screens/forgot_password.dart';
 
 class CreateNewPassword extends StatefulWidget {
-   const CreateNewPassword({super.key});
+  const CreateNewPassword({super.key});
 
   @override
-  State<CreateNewPassword> createState() => _CreateNewPassword();
+  State<CreateNewPassword> createState() => _CreateNewPasswordState();
 }
 
-class _CreateNewPassword extends State<CreateNewPassword> {
-
+class _CreateNewPasswordState extends State<CreateNewPassword> {
   bool obt = true;
-  bool obt2 =true;
-    @override
-    Widget build(BuildContext context){
+  bool obt2 = true;
 
-       const   border =  OutlineInputBorder(
-        borderRadius:  BorderRadius.all(Radius.circular(10)),
-        borderSide:   BorderSide(
-          width: 2.0,
-          style: BorderStyle.solid,
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _newPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void _validateAndSubmit() {
+    if (_newPasswordController.text != _confirmPasswordController.text) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Passwords do not match!'),
+          backgroundColor: Colors.red,
         ),
       );
+    } else {
+      // Handle password update logic here
+      debugPrint('Password updated successfully');
+      // You can navigate to another page or display a success message
+    }
+  }
 
-      return  Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: const Color(0xFFB0BEC5),
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFB0BEC5),
         elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
+            Navigator.pop(context); // Navigate back to ForgotPassword
             debugPrint('Back is pressed');
           },
           color: const Color.fromARGB(255, 11, 106, 14),
@@ -41,107 +65,90 @@ class _CreateNewPassword extends State<CreateNewPassword> {
             color: Color.fromARGB(255, 11, 106, 14),
           ),
         ),
-          centerTitle: true,
-        ),
-
-
-          body : SingleChildScrollView(
-            child: Center(
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //  Text(result.toString(),
-                  // style: const  TextStyle(
-                  //     fontSize: 35,
-                  //     fontWeight: FontWeight.bold,
-                  //     color: Colors.white,
-                  // ),
-                  //  ),
-            
-            
-                  Image.asset("lib/assets/forgot_password.png"),
-            
-                  Container(
-                    padding: const  EdgeInsets.all(8.0),
-                    child:  TextField(   
-                        
-                      style: const TextStyle(
-                        color: Colors.black , 
-                      ),
-                      obscureText: obt,
-                      decoration:  InputDecoration(
-                        hintText:'Enter Your New Password ',
-                        hintStyle: const TextStyle(
-                          color:Colors.black
-                        ),
-                        prefixIcon:  const Icon(Icons.password_sharp),
-                        prefixIconColor: Colors.black,
-                        suffixIcon: IconButton(onPressed: () {
-                          setState(() {
-                            obt =! obt;
-                          });
-                        }, icon: const Icon(Icons.remove_red_eye)),
-                        suffixIconColor: Colors.black,
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: border,
-                        enabledBorder: border,
-                        ),
-                        
-                      keyboardType: TextInputType.visiblePassword,
-                    ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("lib/assets/forgot_password.png"),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _newPasswordController,
+                  style: const TextStyle(
+                    color: Colors.black,
                   ),
-
-                  Container(
-                    padding: const  EdgeInsets.all(8.0),
-                    child:  TextField(   
-                        
-                      style: const TextStyle(
-                        color: Colors.black , 
-                      ),
-                      obscureText: obt2,
-                      decoration:  InputDecoration(
-                        hintText:'Confirm password ',
-                        hintStyle: const TextStyle(
-                          color:Colors.black
-                        ),
-                        prefixIcon:  const Icon(Icons.password_sharp),
-                        prefixIconColor: Colors.black,
-                        suffixIcon: IconButton(onPressed: () {
-                          setState(() {
-                            obt2 =! obt2;
-                          });
-                        }, icon: const Icon(Icons.remove_red_eye)),
-                        suffixIconColor: Colors.black,
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: border,
-                        enabledBorder: border,
-                        ),
-                        
-                      keyboardType: TextInputType.visiblePassword,
-                    ),
-                  ),
-                    
-                   Container(
-                    padding: const EdgeInsets.all(8.0) ,
-                     child: TextButton(
+                  obscureText: obt,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your New Password',
+                    hintStyle: const TextStyle(color: Colors.black),
+                    prefixIcon: const Icon(Icons.password_sharp),
+                    prefixIconColor: Colors.black,
+                    suffixIcon: IconButton(
                       onPressed: () {
-                        debugPrint('CreateNew password pressed');
+                        setState(() {
+                          obt = !obt;
+                        });
                       },
-                      style: TextButton.styleFrom(
-                        backgroundColor: const Color(0xFFB0BEC5),
-                        foregroundColor:  const Color.fromARGB(255, 11, 106, 14),
-                        minimumSize: const Size(double.infinity, 50),
-                      ),
-                       child: const  Text('Update Password'),
-                       
-                       ),
-                   )
-                ],
+                      icon: const Icon(Icons.remove_red_eye),
+                    ),
+                    suffixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
                 ),
-                 ),
-          ), 
-        );
-    }
-} 
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _confirmPasswordController,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  obscureText: obt2,
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    hintStyle: const TextStyle(color: Colors.black),
+                    prefixIcon: const Icon(Icons.password_sharp),
+                    prefixIconColor: Colors.black,
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obt2 = !obt2;
+                        });
+                      },
+                      icon: const Icon(Icons.remove_red_eye),
+                    ),
+                    suffixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: _validateAndSubmit,
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFFB0BEC5),
+                    foregroundColor: const Color.fromARGB(255, 11, 106, 14),
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  child: const Text('Update Password'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
